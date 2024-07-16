141348721540b4448cf3d8e37bc2407c713006b9
Carlos Zabala <czabala847@gmail.com>
2024-07-16 10:34:43 -0500

    enabled cors for test

---
+++ src/routes/auth-routes.ts
@@ -0,0 +1,12 @@
+import { Router } from "express"
+
+const router = Router();
+
+router.get("/", (req, res) => {
+    res.send("Auth")
+})
+
+
+
+
+export default router
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
-import cors from "cors";
-import dotenv from "dotenv";
-import express from "express";
-import { corsConfig } from "./config/cors";
-import { connectDB } from "./config/db";
-import projectRoutes from "./routes/project-routes";
+import cors from "cors"
+import dotenv from "dotenv"
+import express from "express"
+import { corsConfig } from "./config/cors"
+import { connectDB } from "./config/db"
+import authRoutes from "./routes/auth-routes"
+import projectRoutes from "./routes/project-routes"
 
 dotenv.config();
 connectDB();
@@ -12,6 +13,7 @@ app.use(cors(corsConfig));
 app.use(express.json());
 
 // Routes
+app.use("/api/auth", authRoutes);
 app.use("/api/projects", projectRoutes);
 
 export default app;
