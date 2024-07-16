6da52f5fe925e90a7dd557587daffe781e94cd01
Carlos Zabala <czabala847@gmail.com>
2024-07-16 10:22:15 -0500

    enable cors

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
+import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
+import { corsConfig } from "./config/cors";
 import { connectDB } from "./config/db";
 import projectRoutes from "./routes/project-routes";
 
 dotenv.config();
 connectDB();
 const app = express();
+app.use(cors(corsConfig));
 app.use(express.json());
 
 // Routes
