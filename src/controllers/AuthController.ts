ae8dbaabac0fbe96846b41db311c02e64112e929
Carlos Zabala <czabala847@gmail.com>
2024-07-23 17:29:44 -0500

    prevent duplicate users

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -5,7 +5,14 @@ import { hasPassword } from "../utils/auth"
 export class AuthController {
   static createAccount = async (req: Request, res: Response) => {
     try {
-      const { password } = req.body;
+      const { password, email } = req.body;
+
+      const userExists = await User.findOne({ email });
+
+      if (userExists) {
+        return res.status(409).json({ error: "User already exists" });
+      }
+
       const user = new User(req.body);
       user.password = await hasPassword(password);
       await user.save();
