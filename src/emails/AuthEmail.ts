20e9a5065eff072e8688fda8bcc61fd8a920ff7a
Carlos Zabala <czabala847@gmail.com>
2024-08-01 09:50:52 -0500

    change url confirm account

--- src/emails/AuthEmail.ts
+++ src/emails/AuthEmail.ts
@@ -18,7 +18,7 @@ export class AuthEmail {
       html: `<h1>Please confirm your email</h1>
             <p>Hola ${userName}, has creado tu cuenta en Uptask, ya casi esta todo listo, solo debes confirmar tu cuenta</p>
             <p>Visita el siguiente enlace:</p>
-            <a href="">Confirmar cuenta</a>
+            <a href="${process.env.FRONTEND_URL}/auth/confirm-account">Confirmar cuenta</a>
             <p>E ingresa el código de confirmación: <b>${token}</b></p>
             `,
     });
